32f08f7105046018c36a0e06f1e4882f04957ac8
ihaseebullah <ihaseebullahtarakai@gmail.com>
2024-07-30 00:08:57 +0500

    Developed pre hook for deleting user data (comments, forums, posts, and profile)

--- Server/Models/User.js
+++ Server/Models/User.js
@@ -1,39 +1,49 @@
 const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema({
-  username: {
-    type: String,
-    required: true,
-    unique: true,
-    trim: true
-  },
-  email: {
-    type: String,
-    required: true,
-    unique: true,
-    trim: true
-  },
-  password: {
-    type: String,
-    required: true
-  },
-  profilePicture: {
-    type: String,
-    default: ''
-  },
-  bio: {
-    type: String,
-    default: ''
-  },
-  preferences: {
-    type: Map,
-    of: String,
-    default: {}
-  },
-  createdAt: {
-    type: Date,
-    default: Date.now
-  }
+    username: {
+        type: String,
+        required: true,
+        unique: true,
+        trim: true
+    },
+    // email: {
+    //     type: String,
+    //     required: true,
+    //     unique: true,
+    //     trim: true
+    // },
+    // password: {
+    //     type: String,
+    //     required: true
+    // },
+    profilePicture: {
+        type: String,
+        default: ''
+    },
+    bio: {
+        type: String,
+        default: ''
+    },
+    preferences: {
+        type: Map,
+        of: String,
+        default: {}
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now
+    }
 }, { timestamps: true });
+
+// Pre hook to delete related data
+userSchema.pre('remove', async function (next) {
+    const userId = this._id;
+    await mongoose.model('Post').deleteMany({ author: userId });
+    await mongoose.model('Comment').deleteMany({ author: userId });
+    await mongoose.model('Forum').deleteMany({ creator: userId });
+    await mongoose.model('Goal').updateMany({ sharedWith: userId }, { $pull: { sharedWith: userId } });
+    next();
+});
 
 module.exports = mongoose.model('User', userSchema);
