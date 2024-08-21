0ce2f92c0a12aac712bc964d72ca38c128e08ef0
ArfaatHussain <arfaathussain745@gmail.com>
2024-08-21 12:27:45 +0500

    Changed template from typescript to  javascript

---
+++ Client/App.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { View } from "react-native";
+
+
+export default function App(){
+  return(
+    <View>
+      <Text>Social Networking App</Text>
+    </View>
+  )
+}
